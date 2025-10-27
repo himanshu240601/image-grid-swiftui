5925f3afbb9f596bde38693b2cf2acc641c34178
Himanshu Goyal <goyahim69@gmail.com>
2025-10-27 21:36:49 +0530

    - first commit

---
+++ ImageGalleryApp/ImageGalleryApp/ImageGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryAppApp.swift
+//  ImageGalleryApp
+//
+//  Created by Himanshu's MacBook on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
